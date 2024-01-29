93003d44f2cd6ad857b68023efe5fa0139d3b02a
RaquelCollado <rcolladoherrera@gmail.com>
2024-01-29 17:41:50 +0100

    add images

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
-import './styles/App.scss';
 import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
