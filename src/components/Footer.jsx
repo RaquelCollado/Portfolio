b9644ad260601f8381e637ed486239a9f9fafba8
RaquelCollado <rcolladoherrera@gmail.com>
2024-02-05 15:27:32 +0100

    add styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,8 @@
 function Footer() {
   return (
     <div className='footer'>
-      <p className='footer_text'>Raquel Collado Herrera 2024</p>
+      <p className='footer_text'>Raquel Collado Herrera</p>
+      <p className='footer_text'>2024</p>
     </div>
   )
 }
