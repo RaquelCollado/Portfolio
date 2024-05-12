464d58eef426a583ac00af2228ad851e1cd95dd6
RaquelCollado <rcolladoherrera@gmail.com>
2024-05-12 19:27:54 +0200

    import cv

--- src/components/Cv.jsx
+++ src/components/Cv.jsx
@@ -2,6 +2,7 @@
 import { Link } from "react-router-dom";
 import image from "../images/anterior.png";
 import cvitae from "../images/cv.png";
+import cvPdf from "../CV_RaquelCollado.pdf";
 function Cv() {
   return (
     <>
@@ -12,7 +13,7 @@ function Cv() {
         <div className="curriculum">
           <p className="curriculum_text">Pincha en la imagen y descargarás mi curriculum</p>
           <a
-            href="/CV_RaquelCollado.pdf"
+            href={cvPdf}
             download="CV_RaquelCollado.pdf"
             ><img className="curriculum_image" src={cvitae} alt="Curriculum Vitae" />
           </a>
